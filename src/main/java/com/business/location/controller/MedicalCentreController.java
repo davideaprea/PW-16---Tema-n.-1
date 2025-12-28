9c805180de6293609601c5dc8609805db6f531ab
daviaprea <apredav@gmail.com>
2025-12-28 17:44:05 +0100

    medical centre controller

---
+++ src/main/java/com/business/location/controller/MedicalCentreController.java
@@ -0,0 +1,28 @@
+package com.business.location.controller;
+
+import com.business.location.dto.MedicalCentreDTO;
+import com.business.location.entity.MedicalCentre;
+import com.business.location.service.MedicalCentreService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/medical-centres")
+public class MedicalCentreController {
+    private final MedicalCentreService medicalCentreService;
+
+    @PostMapping
+    public ResponseEntity<MedicalCentre> create(@Valid @RequestBody MedicalCentreDTO dto) {
+        return new ResponseEntity<>(
+                medicalCentreService.create(dto),
+                HttpStatus.CREATED
+        );
+    }
+}
