a76453f88f03f207383e38b46fdb183fc9b02616
daviaprea <apredav@gmail.com>
2025-12-29 16:26:10 +0100

    response dto definition

---
+++ src/main/java/com/business/location/dto/response/MedicalCentreCreateResponse.java
@@ -0,0 +1,33 @@
+package com.business.location.dto.response;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record MedicalCentreCreateResponse(
+        long id,
+        String email,
+        String phone,
+        AddressDTO address,
+        List<FloorDTO> floors
+) {
+    public record AddressDTO(
+            long id,
+            String name,
+            String number,
+            BigDecimal latitude,
+            BigDecimal longitude
+    ) {
+    }
+
+    public record FloorDTO(
+            long id,
+            int number,
+            List<RoomDTO> rooms
+    ) {
+        public record RoomDTO(
+                long id,
+                int number
+        ) {
+        }
+    }
+}
