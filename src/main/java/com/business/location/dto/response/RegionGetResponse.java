231081f37d6ebdbbdd1cba457b91f4dd675d48c2
daviaprea <apredav@gmail.com>
2025-12-29 17:25:01 +0100

    get all regions api

---
+++ src/main/java/com/business/location/dto/response/RegionGetResponse.java
@@ -0,0 +1,7 @@
+package com.business.location.dto.response;
+
+public record RegionGetResponse(
+        long id,
+        String name
+) {
+}
--- src/main/java/com/business/location/entity/Region.java
+++ src/main/java/com/business/location/entity/Region.java
@@ -3,12 +3,14 @@ package com.business.location.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @Table(name = "regions")
 @NoArgsConstructor
+@Getter
 public class Region {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
