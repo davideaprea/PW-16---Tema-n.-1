dc22ee673565695e8f2001d905ee00b014a448b5
daviaprea <apredav@gmail.com>
2025-12-29 18:29:51 +0100

    page validation

--- src/main/java/com/business/location/controller/MedicalCentreController.java
+++ src/main/java/com/business/location/controller/MedicalCentreController.java
@@ -5,9 +5,11 @@ import com.business.location.dto.response.MedicalCentreCreateResponse;
 import com.business.location.dto.response.MedicalCentreGetPageResponse;
 import com.business.location.service.MedicalCentreService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,10 +30,27 @@ public class MedicalCentreController {
 
     @GetMapping
     public ResponseEntity<Page<MedicalCentreGetPageResponse>> search(
-            @RequestParam long regionId,
-            @RequestParam(required = false) String city,
-            Pageable pageable
+            @RequestParam
+            long regionId,
+
+            @RequestParam(required = false)
+            String city,
+
+            @RequestParam(defaultValue = "1")
+            @Min(1)
+            @Max(30)
+            int pageSize,
+
+            @RequestParam(defaultValue = "0")
+            @Min(0)
+            int pageNumber
     ) {
-        return ResponseEntity.ok(medicalCentreService.findByRegionAndCity(regionId, city, pageable));
+        return ResponseEntity.ok(
+                medicalCentreService.findByRegionAndCity(
+                        regionId,
+                        city,
+                        PageRequest.of(pageNumber, pageSize)
+                )
+        );
     }
 }
