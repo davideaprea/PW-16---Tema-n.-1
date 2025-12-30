26b1af43f2113fac1b4fad52a461b70b4e3459ad
daviaprea <apredav@gmail.com>
2025-12-30 13:31:23 +0100

    transaction for centre creation

--- src/main/java/com/business/group/location/service/MedicalCentreService.java
+++ src/main/java/com/business/group/location/service/MedicalCentreService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 @AllArgsConstructor
@@ -21,6 +22,7 @@ public class MedicalCentreService {
     private final MedicalCentreRequestMapper medicalCentreRequestMapper;
     private final MedicalCentreResponseMapper medicalCentreResponseMapper;
 
+    @Transactional
     public MedicalCentreCreateResponse create(MedicalCentreCreateRequest dto) {
         MedicalCentre medicalCentreToSave = medicalCentreRequestMapper.toMedicalCentre(dto);
         MedicalCentre savedMedicalCentre = medicalCentreDAO.save(medicalCentreToSave);
