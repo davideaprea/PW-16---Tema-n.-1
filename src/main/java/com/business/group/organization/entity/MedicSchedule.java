47034f0b2f24eb56f426486a4db7df6073514fbf
daviaprea <apredav@gmail.com>
2026-01-05 12:34:36 +0100

    medic schedule entity definition

---
+++ src/main/java/com/business/group/organization/entity/MedicSchedule.java
@@ -0,0 +1,33 @@
+package com.business.group.organization.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Immutable;
+
+import java.time.LocalTime;
+
+@Immutable
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "medic_schedules")
+public class MedicSchedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long medicId;
+
+    @Column(nullable = false)
+    private Long roomId;
+
+    @Column(nullable = false)
+    private LocalTime from;
+
+    @Column(nullable = false)
+    private LocalTime to;
+}
