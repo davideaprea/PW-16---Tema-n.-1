3b0c0b8016140123ce260011b0f7b1616ecb9557
daviaprea <apredav@gmail.com>
2026-01-05 12:38:30 +0100

    medic schedule dao definition

---
+++ src/main/java/com/business/group/organization/dao/MedicScheduleDAO.java
@@ -0,0 +1,7 @@
+package com.business.group.organization.dao;
+
+import com.business.group.organization.entity.MedicSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicScheduleDAO extends JpaRepository<MedicSchedule, Long> {
+}
