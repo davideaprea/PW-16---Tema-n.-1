701bbb05427ce143ea004ee76bd6d9a0efa22ef6
daviaprea <apredav@gmail.com>
2026-01-07 11:35:19 +0100

    centre service creation

---
+++ src/main/java/com/business/group/healthcare/mapper/CentreServiceMapper.java
@@ -0,0 +1,13 @@
+package com.business.group.healthcare.mapper;
+
+import com.business.group.healthcare.dto.CentreServiceCreateRequest;
+import com.business.group.healthcare.dto.CentreServiceCreateResponse;
+import com.business.group.healthcare.entity.CentreService;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CentreServiceMapper {
+    CentreService toEntity(CentreServiceCreateRequest dto);
+
+    CentreServiceCreateResponse toResponse(CentreService centreService);
+}
---
+++ src/main/java/com/business/group/healthcare/service/CentreServiceService.java
@@ -0,0 +1,19 @@
+package com.business.group.healthcare.service;
+
+import com.business.group.healthcare.dao.CentreServiceDAO;
+import com.business.group.healthcare.dto.CentreServiceCreateRequest;
+import com.business.group.healthcare.dto.CentreServiceCreateResponse;
+import com.business.group.healthcare.mapper.CentreServiceMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class CentreServiceService {
+    private final CentreServiceDAO centreServiceDAO;
+    private final CentreServiceMapper centreServiceMapper;
+
+    public CentreServiceCreateResponse create(CentreServiceCreateRequest dto) {
+        return centreServiceMapper.toResponse(centreServiceDAO.save(centreServiceMapper.toEntity(dto)));
+    }
+}
