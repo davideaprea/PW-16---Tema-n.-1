60dd94c1677f869bf79761335853de0bc946e521
daviaprea <apredav@gmail.com>
2026-01-12 14:37:58 +0100

    resource not found exception

--- src/main/java/com/business/group/healthcare/service/RoomMedicalCareService.java
+++ src/main/java/com/business/group/healthcare/service/RoomMedicalCareService.java
@@ -4,6 +4,7 @@ import com.business.group.healthcare.dao.RoomMedicalCareDAO;
 import com.business.group.healthcare.dto.RoomMedicalCareCreateRequest;
 import com.business.group.healthcare.dto.RoomMedicalCareCreateResponse;
 import com.business.group.healthcare.dto.RoomMedicalCareGetResponse;
+import com.business.group.healthcare.exception.RoomMedicalCareNotFoundException;
 import com.business.group.healthcare.mapper.RoomMedicalCareMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,6 @@ public class RoomMedicalCareService {
     public RoomMedicalCareGetResponse getByMedicalCareIdAndRoomId(long medicalCareId, long roomId) {
         return roomMedicalCareMapper.toRoomServiceGetResponse(roomMedicalCareDAO
                 .findByMedicalCareIdAndRoomId(medicalCareId, roomId)
-                .orElseThrow());
+                .orElseThrow(() -> new RoomMedicalCareNotFoundException(medicalCareId, roomId)));
     }
 }
